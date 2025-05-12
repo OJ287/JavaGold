e0ae10bf755effc72f597897c49c0dc1ac1e4903
OJ287 <enhouri@gmail.com>
2025-05-12 23:46:53 +0900

    二次复习中
    Signed-off-by: OJ287 <enhouri@gmail.com>

---
+++ src/Memo/A01_PackageList.java
@@ -0,0 +1,85 @@
+package Memo;
+
+/**
+ * @author liyanpeng
+ * @date 2025/5/12
+ * @description TODO
+ */
+
+/**
+ * 🧠 Java 工具类一览表（常见包分类 + 功能分类）
+ * ┌──────────────────────┬──────────────┬────────────┐
+ * │ パッケージ            │ 名称         │ 種別         │
+ * ├──────────────────────┼──────────────┼────────────┤
+ * │ java.lang            │ Object       │ class      │
+ * │                      │ String       │ class      │
+ * │                      │ StringBuilder│ class      │
+ * │                      │ StringBuffer │ class      │
+ * │                      │ Math         │ class      │
+ * │                      │ System       │ class      │
+ * │                      │ Integer      │ class      │
+ * │                      │ Double       │ class      │
+ * │                      │ Boolean      │ class      │
+ * │                      │ Character    │ class      │
+ * │                      │ Enum         │ class      │
+ * │                      │ Class        │ class      │
+ * │                      │ Runnable     │ interface  │
+ * │                      │ Comparable   │ interface  │
+ * │                      │ CharSequence │ interface  │
+ * │                      │ AutoCloseable│ interface  │
+ * │                      │ Override     │ annotation │
+ * │                      │ Deprecated   │ annotation │
+ * │                      │ SuppressWarnings │ annotation │
+ * ├──────────────────────┼──────────────┼────────────┤
+ * │ java.util            │ List         │ interface  │
+ * │                      │ Set          │ interface  │
+ * │                      │ Map          │ interface  │
+ * │                      │ Queue        │ interface  │
+ * │                      │ Collection   │ interface  │
+ * │                      │ Iterator     │ interface  │
+ * │                      │ Arrays       │ class      │
+ * │                      │ Collections  │ class      │
+ * │                      │ ArrayList    │ class      │
+ * │                      │ LinkedList   │ class      │
+ * │                      │ HashSet      │ class      │
+ * │                      │ TreeSet      │ class      │
+ * │                      │ HashMap      │ class      │
+ * │                      │ TreeMap      │ class      │
+ * │                      │ Hashtable    │ class      │
+ * │                      │ Optional     │ class      │
+ * │                      │ Comparator   │ interface  │
+ * │                      │ Objects      │ class      │
+ * ├──────────────────────┼──────────────┼────────────┤
+ * │ java.util.function   │ Function     │ interface  │
+ * │                      │ Predicate    │ interface  │
+ * │                      │ Consumer     │ interface  │
+ * │                      │ Supplier     │ interface  │
+ * │                      │ UnaryOperator│ interface  │
+ * │                      │ BinaryOperator│ interface │
+ * ├──────────────────────┼──────────────┼────────────┤
+ * │ java.util.stream     │ Stream       │ interface  │
+ * │                      │ IntStream    │ interface  │
+ * │                      │ Collectors   │ class      │
+ * │                      │ Collector    │ interface  │
+ * ├──────────────────────┼──────────────┼────────────┤
+ * │ java.time            │ LocalDate    │ class      │
+ * │                      │ LocalTime    │ class      │
+ * │                      │ LocalDateTime│ class      │
+ * │                      │ ZonedDateTime│ class      │
+ * │                      │ Period       │ class      │
+ * │                      │ Duration     │ class      │
+ * │                      │ DateTimeFormatter │ class │
+ * │                      │ ZoneId       │ class      │
+ * ├──────────────────────┼──────────────┼────────────┤
+ * │ java.text            │ NumberFormat │ class      │
+ * │                      │ DecimalFormat│ class      │
+ * │                      │ DateFormat   │ class      │
+ * │                      │ SimpleDateFormat │ class  │ ※已过时，但考察理解
+ * └──────────────────────┴──────────────┴────────────┘
+ */
+public class A01_PackageList {
+    // 自动生成 main 方法
+    public static void main(String[] args) {
+        // TODO
+    }
+}
